5981d2a818540ecf289b1863b870534d983919a7
ayushjamloki <ayushjamloki8@gmail.com>
2025-04-04 13:48:41 +0530

    Add files via upload

---
+++ Project Telemedicine/index.js
@@ -0,0 +1,9 @@
+function openChatbotPage() {
+    window.location.href = "medbot.html"; // This opens the chatbot page
+}
+function openPatientLogin() {
+    window.location.href = "patientlogin.html"; // This opens the chatbot page
+}
+function openDoctorLogin() {
+    window.location.href = "doctorlogin.html"; // This opens the chatbot page
+}
